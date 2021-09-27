4600974ce2add4cd397094ab9bd31314bcf4abbe
Uislan Cruz <uislansk8@hotmail.com>
2021-09-27 20:04:43 -0300

    Inicio
    Aqui apenas uma soma simples de duas variáveis, e retirando sua media.

---
+++ OperadoresAritimeticos.java
@@ -0,0 +1,20 @@
+package operadoresaritimeticos;
+
+/**
+ *
+ * @author uisla
+ */
+public class OperadoresAritimeticos {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n1 = 3;
+        int n2 = 5;
+        float m = (n1 + n2)/2;
+        System.out.println("A media é igual a: " +m);
+        
+               
+    }
+    
+}
