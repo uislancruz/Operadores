f7c7d4b1e69cde3475d86b033a220b4781771632
Uislan Cruz <uislansk8@hotmail.com>
2021-09-29 21:18:54 -0300

    Fazendo random
    Usando random para fazer numeros aleatorio entre um intervalo determinado.

--- OperadoresAritimeticos/src/operadoresaritimeticos/OperadoresAritimeticos.java
+++ OperadoresAritimeticos/src/operadoresaritimeticos/OperadoresAritimeticos.java
@@ -21,13 +21,16 @@ public class OperadoresAritimeticos {
         float m = (n1 + n2)/2;
         System.out.println("A media é igual a: " +m);*/
        
-       float v = 8.3f;
-       int ar = (int)Math.round(v);
-        System.out.println(ar);
+       //float v = 8.3f;
+       //int ar = (int)Math.round(v);
+        //System.out.println(ar);
         
        //para melhor entendimento, melhor sempre usar o round, pois ele faz arredondamento aritimeticos, ex: 8,5 = 9 ou 8,4 igual a 8
        // temos ciel que arredonda para cima e o floor que arredonda para baixo
         
+       double ale = Math.random();
+       int n = (int) (5 + ale * (10-5));
+        System.out.println(n);
                
     }
     
